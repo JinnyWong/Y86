ee04cc4c70246bf298a0d1702b04dc222893ecd1
HuiShann <90441325+HuiShann@users.noreply.github.com>
2022-12-15 01:15:52 +0800

    vue

---
+++ gui/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="overflow-scroll">
+<div
+  class="bg-image overflow-scroll"
+  style="
+    background-image: url('https://images.unsplash.com/photo-1530177150700-84cd9a3b059b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80');
+    background-attachment: fixed;
+    height: 100vh;
+  "
+>
+   <Header></Header>
+    <div class="container">
+      <div class="row mt-5">
+        <Card></Card>
+      </div>
+    </div>
+    <Footer></Footer>
+</div>
+</div>
+</template>
+
+<script>
+
+
+//importing bootstrap 5 Modules
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Header from './components/MyHeader'
+import Card from './components/MyCard'
+export default {
+  
+components: {
+    Header,
+    Card,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+
+</style>
---
+++ gui/src/components/MyHeader.vue
@@ -0,0 +1,63 @@
+
+<template>
+<div class="fixed-top">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-hide-on-scroll">
+    <div class="container">
+      <a class="navbar-brand" href="#">Y86 Simulation</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Start</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" role="button">Next</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" role="button">Reset</a>
+          </li>
+        </ul>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#3E3E3D" class="bi bi-circle-fill" viewBox="0 0 16 16">
+  <circle cx="8" cy="8" r="8"/>
+</svg>
+<div class="me-2"></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#DFDEDC" class="bi bi-circle-fill" viewBox="0 0 16 16">
+  <circle cx="8" cy="8" r="8"/>
+</svg>
+  <div class="me-2"></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#DFDEDC" class="bi bi-circle-fill" viewBox="0 0 16 16">
+  <circle cx="8" cy="8" r="8"/>
+</svg>
+  <div class="me-2"></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#C7C6C4" class="bi bi-circle-fill" viewBox="0 0 16 16">
+  <circle cx="8" cy="8" r="8"/>
+</svg>
+      </div>
+    </div>
+  </nav>
+</div>
+</template>
+
+
+<style>
+.btn-bd-primary {
+  --bs-btn-font-weight: 600;
+  --bs-btn-color: var(#434242);
+  --bs-btn-bg: transparent;
+  --bs-btn-border-color: var(--bd-violet);
+  --bs-btn-border-radius: 0.5rem;
+  --bs-btn-hover-color: var(--bs-white);
+  --bs-btn-hover-bg: #{shade-color($bd-violet, 10%)};
+  --bs-btn-hover-border-color: #{shade-color($bd-violet, 10%)};
+  --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+  --bs-btn-active-color: var(--bs-btn-hover-color);
+  --bs-btn-active-bg: #{shade-color($bd-violet, 20%)};
+  --bs-btn-active-border-color: #{shade-color($bd-violet, 20%)};
+}
+
+.navbar-custom {
+    background-color: #f9f9f9;
+}
+</style>
